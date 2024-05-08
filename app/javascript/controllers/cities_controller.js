032439f0c582e1f5dc23a5b3f607c4b6a2152bfe
Krist Menina <krist@Favorite-Actress.local>
2024-05-08 14:48:57 +0800

    request.js

--- app/javascript/controllers/cities_controller.js
+++ app/javascript/controllers/cities_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import { FetchRequest } from "@rails/request.js"
 
 export default class extends Controller {
   connect() {
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.9
