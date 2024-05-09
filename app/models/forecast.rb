a72f48750f7781448f7193f17adc9d817888b585
Krist Menina <krist@Favorite-Actress.local>
2024-05-09 12:14:12 +0800

    refactor forecast

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -10,38 +10,17 @@ class Forecast < ApplicationRecord
     self.sunset_times = serialize(data["sunset"])
   end
 
-  def weather_codes
-    codes.split("|").map { |code| code.to_i }
-  end
-
   def seven_day_forecast
     temps ||= temps_min_max
-    sunrises ||= sunrise_sunset
     {
       "city" => city.name,
-      "time" => dates,
-      "weather_code" => weather_codes,
-      "temperature_2m_min" => temps[:min],
-      "temperature_2m_max" => temps[:max],
-      "sunrise" => sunrises[:sunrises],
-      "sunset" => sunrises[:sunsets]
+      "time" => unserialize(days),
+      "weather_code" => unserialize(codes).map(&:to_i),
+      "temperature_2m_min" => unserialize(min_temps)&.map(&:to_f),
+      "temperature_2m_max" => unserialize(max_temps)&.map(&:to_f),
+      "sunrise" => unserialize(sunrise_times),
+      "sunset" => unserialize(sunset_times)
     }
-  end
-
-  def temps_min_max
-    min = min_temps.split("|").map(&:to_f) unless min_temps.nil?
-    max = max_temps.split("|").map(&:to_f) unless max_temps.nil?
-    {min: min, max: max}
-  end
-
-  def sunrise_sunset
-    sunrises = sunrise_times.split("|") unless sunrise_times.nil?
-    sunsets = sunset_times.split("|") unless sunset_times.nil?
-    {sunrises: sunrises, sunsets: sunsets}
-  end
-
-  def dates
-    days.split("|") unless days.nil?
   end
 
   private
