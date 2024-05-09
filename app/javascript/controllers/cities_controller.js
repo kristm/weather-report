eb30aaed5bf44d33d5c7959964d97756a4efba28
Krist Menina <krist@Favorite-Actress.local>
2024-05-09 13:07:56 +0800

    prevent hyperlink from trying to render whole page

--- app/javascript/controllers/cities_controller.js
+++ app/javascript/controllers/cities_controller.js
@@ -11,6 +11,6 @@ export default class extends Controller {
     coords['long'] = event.target.getAttribute('data-long');
     const data = {coords: coords};
     post('/forecast', { body: data, responseKind: 'turbo-stream' });
-    console.log(data, event.target.getAttribute('data-lat'));
+    event.preventDefault();
   }
 }
