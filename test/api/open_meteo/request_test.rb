bce46431f7df0a25791c92b9c644b851bff2b2e2
Krist Menina <krist@Favorite-Actress.local>
2024-05-10 09:05:02 +0800

    use minitest

--- test/api/open_meteo/request_test.rb
+++ test/api/open_meteo/request_test.rb
@@ -1,15 +1,17 @@
 require 'test_helper'
 
-class OpenMeteo::RequestTest < ActiveSupport::TestCase
-  test "can make request to the api" do
-    WebMock.allow_net_connect!
-    resp = OpenMeteo::Request.forecast(35.45,139.45)
-    assert_requested :get, "https://api.open-meteo.com/v1/forecast?latitude=35.45&longitude=139.45&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset"
-  end
+describe "OpenMeteo::Request" do
+  describe ".forecast" do
+    it "can make request to the api" do
+      WebMock.allow_net_connect!
+      resp = OpenMeteo::Request.forecast(35.45,139.45)
+      assert_requested :get, "https://api.open-meteo.com/v1/forecast?latitude=35.45&longitude=139.45&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset"
+    end
   
-  test "request success" do
-    WebMock.allow_net_connect!
-    resp = OpenMeteo::Request.forecast(35.45,139.45)
-    assert_equal 200, resp.code, "request ok"
+    it "request success" do
+      WebMock.allow_net_connect!
+      resp = OpenMeteo::Request.forecast(35.45,139.45)
+      assert_equal 200, resp.code, "request ok"
+    end
   end
 end
