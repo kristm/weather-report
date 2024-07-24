09e50564bef04266cb3e3fd6133be36b8a361879
Krist Menina <krist@Favorite-Actress.local>
2024-07-24 20:04:54 +0800

    remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/forecast' => 'home#forecast'
   post '/forecast' => 'home#forecast'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
